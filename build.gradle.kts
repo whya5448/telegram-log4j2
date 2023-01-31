bc9024f9fc241d190a117cfed45700408b7d7f90
작은모코코 <Whya5448@gmail.com>
2023-01-31 12:16:29 +0900

    spring boot 3.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     kotlin("jvm") version "1.8.0"
     kotlin("plugin.spring") version "1.8.0"
     id("io.spring.dependency-management") version "1.1.0"
-    id("org.springframework.boot") version "2.6.7"
+    id("org.springframework.boot") version "3.0.2"
 }
 
 group = "org.metalscraps.log"
